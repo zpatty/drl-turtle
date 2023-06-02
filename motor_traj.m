7eaaa9f85060fcf96bed8cba7a5c50c8222adcad
Zach <zpatt720@gmail.com>
2023-06-02 14:52:34 -0400

    latest trajectory generator

--- motor_traj.m
+++ motor_traj.m
@@ -3,12 +3,12 @@ theta = [0 0.1 0.2 0.3;
         0 0.1 0.2 0.3;
         0 -0.1 -0.2 -0.3];
 
-shoulder1 = [180 200 270 180 160 180];
-shoulder2 = [180 150 150 220 200 180];
-shoulder3 = repmat(180,1,length(shoulder2));
-shoulder4 = (180 + (180 - shoulder1)) - (180-50);
+shoulder1 = [180 160 90 180	200	180];
+shoulder2 = [180 210 210 140 160 180];
+shoulder3 = repmat(270,1,length(shoulder2));
+shoulder4 = 180 + (180 - shoulder1);
 shoulder5 = 180 + (180 - shoulder2);
-shoulder6 = repmat(180,1,length(shoulder2));
+shoulder6 = repmat(0,1,length(shoulder2));
 
 timePoints = [0 0.5 1 2.5 3 3.5];
 
