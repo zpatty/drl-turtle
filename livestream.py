77cd134b333cee7597cd6b113483a0ff4d29f986
lisakondrich <kondrich@mit.edu>
2023-08-11 13:28:11 -0400

    added fps calculation

--- livestream.py
+++ livestream.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
+import time
 
 # Load YOLO model with pre-trained weights
-net = cv2.dnn.readNet("/Users/lisa/Desktop/turtle/trained_model_info/yolov4-tiny-turtle_1.cfg", "/Users/lisa/Desktop/turtle/trained_model_info/yolov4-tiny-turtle_5000.weights")
+net = cv2.dnn.readNet(".../turtle.cfg", ".../turtle_7000.weights")
 
 # Load COCO labels for Tiny YOLO
-with open("/Users/lisa/Desktop/turtle/trained_model_info/turtle.names", "r") as f:
+with open(".../turtle.names", "r") as f:
     classes = f.read().strip().split("\n")
 
 # Get output layer names (specific to YOLO Tiny)
@@ -13,6 +14,7 @@ output_layers = net.getUnconnectedOutLayersNames()
 
 # Function to perform object detection on an image
 def detect_objects(image):
+
     height, width = image.shape[:2]
     blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), swapRB=True, crop=False)
     net.setInput(blob)
@@ -27,7 +29,7 @@ def detect_objects(image):
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.5:
+            if confidence > 0:
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
@@ -56,8 +58,17 @@ def detect_objects(image):
 
 
 # Function to play video and perform object detection in real-time
-def play_video_with_detection(video_path):
+def play_video_with_detection(video_path = "camera"):
+
+    # Detect from camera instead of video
+    if video_path == "camera":
+        video_path = 0
+        print(video_path)
+
     cap = cv2.VideoCapture(video_path)
+
+    frame_count = 0
+    total_time = 0
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -65,7 +76,14 @@ def play_video_with_detection(video_path):
             break
 
         # Perform object detection on the frame
-        frame = detect_objects(frame)
+        start_time = time.time()
+        frame = detect_objects(frame, True)
+        end_time = time.time()
+
+        # Calculate the time taken for detection in the current frame
+        elapsed_time = end_time - start_time
+        total_time += elapsed_time
+        frame_count += 1
 
         # Display the frame
         cv2.imshow("Object Detection", frame)
@@ -76,6 +94,11 @@ def play_video_with_detection(video_path):
     cap.release()
     cv2.destroyAllWindows()
 
+    # Calculate the average time taken per frame
+    avg_time_per_frame = total_time / frame_count
+    frames_per_second = 1/avg_time_per_frame
+    print("Average fps:", frames_per_second)
+
 if __name__ == "__main__":
-    video_path = "/Users/lisa/Desktop/turtle/live_detection/live_stream_test_video_low_qual.mov"  # Replace with your video file path
+    video_path = ".../live_stream_test_short.mov"  # Replace with your video file path
     play_video_with_detection(video_path)
