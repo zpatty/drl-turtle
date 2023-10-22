528fb40a3bedcb90ee8e3b2034bd050fc6606b66
gracexu7 <gracexu@mit.edu>
2023-10-21 20:34:36 -0400

    launch

--- ros/py_pubsub/launch/initial.py
+++ ros/py_pubsub/launch/initial.py
@@ -5,13 +5,11 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='py_pubsub',
-            namespace='control',
             executable='control',
             name='control'
         ),
         Node(
             package='py_pubsub',
-            namespace='motors',
             executable='motors',
             name='motors'
         )
