254cd5e63cca9933710a1caf4e7f178e7be516f4
gracexu7 <gracexu@mit.edu>
2023-10-23 11:09:23 -0400

    demos

--- ros/py_pubsub/py_pubsub/master.py
+++ ros/py_pubsub/py_pubsub/master.py
@@ -5,6 +5,60 @@ from rclpy.executors import Executor, MultiThreadedExecutor
 from std_msgs.msg import String
 from std_msgs.msg import Float64MultiArray
 
+from __future__ import print_function
+import os
+import json
+import traceback
+
+ESC_ASCII_VALUE             = 0x1b
+SPACE_ASCII_VALUE           = 0x20
+WKEY_ASCII_VALUE            = 0x77
+SKEY_ASCII_VALUE            = 0x73
+AKEY_ASCII_VALUE            = 0x61
+DKEY_ASCII_VALUE            = 0x64
+CKEY_ASCII_VALUE            = 0x63
+BKEY_ASCII_VALUE            = 0x62      # key to bend the top module
+UKEY_ASCII_VALUE            = 0x75      # key to unbend the modules
+NKEY_ASCII_VALUE            = 0x6E
+IKEY_ASCII_VALUE            = 0x69     
+QKEY_ASCII_VALUE            = 0x71 
+TKEY_ASCII_VALUE            = 0x74     
+MOD1_VALUE                  = 0x31      # pressing 1 on keyboard
+MOD2_VALUE                  = 0x32
+MOD3_VALUE                  = 0x33
+
+demo1 = False
+demo2 = False
+stop = False 
+
+import termios, fcntl, sys, os
+from select import select
+fd = sys.stdin.fileno()
+old_term = termios.tcgetattr(fd)
+new_term = termios.tcgetattr(fd)
+
+def getch():
+    new_term[3] = (new_term[3] & ~termios.ICANON & ~termios.ECHO)
+    termios.tcsetattr(fd, termios.TCSANOW, new_term)
+    try:
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_term)
+    return ch
+
+def kbhit():
+    new_term[3] = (new_term[3] & ~(termios.ICANON | termios.ECHO))
+    termios.tcsetattr(fd, termios.TCSANOW, new_term)
+    try:
+        dr,dw,de = select([sys.stdin], [], [], 0)
+        if dr != []:
+            return 1
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_term)
+        sys.stdout.flush()
+
+    return 0
+
 
 class MinimalPublisher(Node):
 
@@ -22,10 +76,36 @@ class MinimalPublisher(Node):
         self.get_logger().info('Publishing: ' +msg.data)
         self.i += 1
 
+class DynamicPublisher(Node):
+
+    def __init__(self, topic):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, topic, 10)
+        timer_period = 1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0.
+
+    def timer_callback(self):
+        msg = String()
+        if demo1:
+            msg.data = 'Running Swimming Trajectory'
+            demo1 = False
+        elif demo2:
+            msg.data = 'Running Walking Trajectory'
+            demo2 = False
+        elif stop:
+            msg.data = 'stop'
+            stop = False
+        else:
+            msg.data = "test " + str(self.i)
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: ' +msg.data)
+        self.i += 1
+
 def main(args=None):
     rclpy.init(args=args)
 
-    tomotors = MinimalPublisher('master_motors')
+    tomotors = DynamicPublisher('master_motors')
     tocv = MinimalPublisher('master_cv')
 
     executor = MultiThreadedExecutor(num_threads=2)
@@ -34,6 +114,29 @@ def main(args=None):
     
     executor.spin()
     
+    try: 
+        while 1:
+            print("\nT: Swimming Trajectory, P: Walking Trajectory, (or press SPACE to quit!)")
+            key_input = getch()
+            if key_input == chr(SPACE_ASCII_VALUE):
+                print("we're quitting\n")
+                stop = True
+                break           
+                
+            elif key_input == chr(WKEY_ASCII_VALUE) or key_input == chr(TKEY_ASCII_VALUE):    # print out the length changes
+                if key_input == chr(TKEY_ASCII_VALUE):
+                    print("\nRunning Swimming Trajectory")
+                    demo1 = True
+                else:
+                    print("\nRunning Walking Trajectory")
+                    demo2 = True
+
+        print("[END OF PROGRAM] Disabling torque\n")
+        # Disable Dynamixel Torque
+    except Exception:
+        print("[ERROR] Disabling torque\n")
+        traceback.print_exc()
+
     rclpy.shutdown()
 
 
--- ros/py_pubsub/py_pubsub/motors.py
+++ ros/py_pubsub/py_pubsub/motors.py
@@ -20,12 +20,31 @@ class MinimalSubscriber(Node):
     def listener_callback(self, msg):
         self.get_logger().info('I heard:' + msg.data)
 
+class DynamicSubscriber(Node):
+
+    def __init__(self, topic):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            topic,
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        if msg.data == 'Running Swimming Trajectory':
+            print('performing demo 1')
+        elif msg.data == 'Running Walking Trajectory':
+            print('performing demo 2')
+        elif msg.data == 'stop':
+            print('stopping')
+        self.get_logger().info('I heard:' + msg.data)
 
 def main(args=None):
     rclpy.init(args=args)
 
     from_cv = MinimalSubscriber('cv_motors')
-    from_master = MinimalSubscriber('master_motors')
+    from_master = DynamicSubscriber('master_motors')
 
     executor = MultiThreadedExecutor(num_threads=2)
     executor.add_node(from_cv)
