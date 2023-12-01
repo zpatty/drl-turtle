3746fdec3f043b12fc21eb89efe48fff9ebfde10
gracexu7 <gracexu@mit.edu>
2023-12-01 12:25:20 -0500

    test1

--- ros/py_pubsub/py_pubsub/sonar.py
+++ ros/py_pubsub/py_pubsub/sonar.py
@@ -19,7 +19,7 @@ class MinimalPublisher(Node):
     def __init__(self, topic):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(String, topic, 10)
-        timer_period = 0.3  # seconds
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
